7130fc19d8a96acbf3da2bb74415afbd9d3fb154
Ales Nosek <ales.nosek@gmail.com>
2021-12-03 19:36:54 -1000

    Test Machine with unresolved tokens is deleted

--- controllers/machine_controller_test.go
+++ controllers/machine_controller_test.go
@@ -4,25 +4,74 @@ import (
 	"testing"
 	"time"
 
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 	"github.com/stretchr/testify/assert"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	machineapi "github.com/openshift/api/machine/v1beta1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 )
 
 func TestDeleteMachineNow(t *testing.T) {
 	assert := assert.New(t)
 
+	mr := NewMachineReconciler(MachineReconcilerConfig{})
 	var machine *unstructured.Unstructured
 
 	machine = &unstructured.Unstructured{Object: map[string]interface{}{}}
-	machine.SetCreationTimestamp(v1.Now())
-	assert.Equal(false, deleteMachineNow(logger, machine))
+	machine.SetCreationTimestamp(metav1.Now())
+	assert.Equal(false, mr.deleteMachineNow(logger, machine))
 
 	machine = &unstructured.Unstructured{Object: map[string]interface{}{}}
-	machine.SetCreationTimestamp(v1.NewTime(time.Now().Add(-50 * time.Second)))
-	assert.Equal(false, deleteMachineNow(logger, machine))
+	machine.SetCreationTimestamp(metav1.NewTime(time.Now().Add(-50 * time.Second)))
+	assert.Equal(false, mr.deleteMachineNow(logger, machine))
 
 	machine = &unstructured.Unstructured{Object: map[string]interface{}{}}
-	machine.SetCreationTimestamp(v1.NewTime(time.Now().Add(-2 * time.Minute)))
-	assert.Equal(true, deleteMachineNow(logger, machine))
+	machine.SetCreationTimestamp(metav1.NewTime(time.Now().Add(-2 * time.Minute)))
+	assert.Equal(true, mr.deleteMachineNow(logger, machine))
 }
+
+var _ = Describe("Machine controller", func() {
+
+	Context("When Machine has unresolved tokens", func() {
+		It("Should delete the Machine", func() {
+			By("Defining a Machine with unresolved tokens")
+			machine := &machineapi.Machine{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "machine.openshift.io/v1beta1",
+					Kind:       "Machine",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "machine1",
+					Namespace: "openshift-machine-api",
+					Annotations: map[string]string{
+						"gitops-friendly-machinesets.redhat-cop.io/enabled": "true"},
+					Labels: map[string]string{
+						"machine.openshift.io/cluster-api-cluster": "INFRANAME",
+					},
+				},
+				Spec: machineapi.MachineSpec{},
+			}
+			By("Creating a Machine with unresolved tokens in Kubernetes")
+			err := k8sClient.Create(ctx, machine, &client.CreateOptions{})
+			Expect(err).ToNot(HaveOccurred())
+			Eventually(func() error {
+				return k8sClient.Get(ctx,
+					types.NamespacedName{Namespace: machine.GetNamespace(), Name: machine.GetName()},
+					machine)
+			}).ShouldNot(HaveOccurred())
+			By("Waiting until the Machine has been deleted")
+			Eventually(func() bool {
+				err := k8sClient.Get(ctx,
+					types.NamespacedName{Namespace: machine.GetNamespace(), Name: machine.GetName()},
+					machine)
+				return apierrors.IsNotFound(err)
+			}).Should(BeTrue())
+		})
+	})
+
+})
--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -17,13 +17,21 @@ limitations under the License.
 package controllers
 
 import (
+	"context"
 	"path/filepath"
 	"testing"
+	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"k8s.io/client-go/kubernetes/scheme"
-	"k8s.io/client-go/rest"
+	machineapi "github.com/openshift/api/machine/v1beta1"
+	"go.uber.org/zap/zapcore"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
@@ -35,9 +43,12 @@ import (
 // These tests use Ginkgo (BDD-style Go testing framework). Refer to
 // http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
 
-var cfg *rest.Config
-var k8sClient client.Client
-var testEnv *envtest.Environment
+var (
+	k8sClient client.Client
+	testEnv   *envtest.Environment
+	ctx       context.Context
+	ctxCancel context.CancelFunc
+)
 
 func TestAPIs(t *testing.T) {
 	RegisterFailHandler(Fail)
@@ -48,28 +59,104 @@ func TestAPIs(t *testing.T) {
 }
 
 var _ = BeforeSuite(func() {
-	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
 
-	By("bootstrapping test environment")
+	const (
+		apiTimeout  = 5 * time.Second
+		apiDuration = 5 * time.Second
+		apiInterval = 250 * time.Millisecond
+	)
+
+	SetDefaultConsistentlyDuration(apiDuration)
+	SetDefaultConsistentlyPollingInterval(apiInterval)
+	SetDefaultEventuallyTimeout(apiTimeout)
+	SetDefaultEventuallyPollingInterval(apiInterval)
+
+	ctx, ctxCancel = context.WithCancel(context.TODO())
+
+	logf.SetLogger(zap.New(
+		zap.WriteTo(GinkgoWriter),
+		zap.UseDevMode(true),
+		zap.Level(zapcore.Level(-10))))
+
+	By("Bootstrapping test environment")
 	testEnv = &envtest.Environment{
-		CRDDirectoryPaths:     []string{filepath.Join("..", "config", "crd", "bases")},
-		ErrorIfCRDPathMissing: false,
+		CRDDirectoryPaths: []string{
+			filepath.Join("..", "config", "crd", "bases"),
+			filepath.Join("..", "test", "crds")},
+		ErrorIfCRDPathMissing:    false,
+		AttachControlPlaneOutput: false,
 	}
-
 	cfg, err := testEnv.Start()
 	Expect(err).NotTo(HaveOccurred())
 	Expect(cfg).NotTo(BeNil())
 
 	//+kubebuilder:scaffold:scheme
 
-	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
+	By("Creating a scheme")
+	scheme := runtime.NewScheme()
+	err = clientgoscheme.AddToScheme(scheme)
+	Expect(err).ToNot(HaveOccurred())
+	err = machineapi.Install(scheme)
+	Expect(err).ToNot(HaveOccurred())
+
+	By("Creating a Kubernetes client")
+	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme})
 	Expect(err).NotTo(HaveOccurred())
 	Expect(k8sClient).NotTo(BeNil())
 
+	By("Creating a openshift-machine-api namespace")
+	ns := &v1.Namespace{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "v1",
+			Kind:       "Namespace",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "openshift-machine-api",
+		},
+	}
+	err = k8sClient.Create(ctx, ns, &client.CreateOptions{})
+	Expect(err).ToNot(HaveOccurred())
+	Eventually(func() error {
+		return k8sClient.Get(ctx, types.NamespacedName{Name: ns.GetName()}, ns)
+	}).ShouldNot(HaveOccurred())
+
+	By("Creating a controller manager")
+	mgr, err := ctrl.NewManager(cfg, ctrl.Options{
+		Scheme: scheme,
+	})
+	Expect(err).ToNot(HaveOccurred())
+
+	controllerName := "gitops-friendly-machinesets"
+
+	err = (&MachineSetReconciler{
+		Client:             mgr.GetClient(),
+		Scheme:             mgr.GetScheme(),
+		EventRecorder:      mgr.GetEventRecorderFor(controllerName),
+		InfrastructureName: "cluster-test-xyz",
+	}).SetupWithManager(mgr)
+	Expect(err).ToNot(HaveOccurred())
+
+	err = (NewMachineReconciler(MachineReconcilerConfig{
+		Client:        mgr.GetClient(),
+		Scheme:        mgr.GetScheme(),
+		EventRecorder: mgr.GetEventRecorderFor(controllerName),
+	},
+		func(mr *machineReconciler) {
+			mr.DeleteMachineMinAgeSeconds = -1
+		})).SetupWithManager(mgr)
+	Expect(err).ToNot(HaveOccurred())
+
+	go func() {
+		defer GinkgoRecover()
+		err = mgr.Start(ctx)
+		Expect(err).ToNot(HaveOccurred(), "Failed to run manager")
+	}()
 }, 60)
 
 var _ = AfterSuite(func() {
-	By("tearing down the test environment")
+	By("Stopping the controller manager")
+	ctxCancel()
+	By("Tearing down the test environment")
 	err := testEnv.Stop()
 	Expect(err).NotTo(HaveOccurred())
 })
